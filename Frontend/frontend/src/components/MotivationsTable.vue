80c76f1fd0c1d00f6e99477f6d4a34ebd7156a18
PiaLember <pia.lember@gmail.com>
2024-12-10 13:03:36 +0200

    Add Frontend

---
+++ Frontend/frontend/src/components/MotivationsTable.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    name: "MotivationsTable",
+    props: {
+        items: Array
+    }
+}
+</script>
+
+<template>
+    <table class="table table-striped">
+        <thead class="table-dark">
+            <tr>
+            <th>Quote</th>
+            <th>Date</th>
+            <th>Likes</th>
+            </tr>
+        </thead>
+    <tbody>
+        <tr v-for="item in items" :key="item.QuoteID">
+            <td>{{ item.Quote }}</td>
+            <td>{{ item.date }}</td>
+            <td>{{ item.likes }}</td>
+        </tr>
+</tbody>
+    </table>
+</template>
---
+++ Frontend/frontend/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script>
+import MotivationsTable from '../components/MotivationsTable.vue';
+import GamesTable from '../components/MotivationsTable.vue'
+export default {
+  components: { MotivationsTable },
+  data() { return {
+    allMotivations: []
+  }},
+  async Created() {
+    this.allMotivations = await (await fetch('http://localhost8080/motivations')).json()
+  }
+}
+</script>
+
+<template>
+  <main>
+    <MotivationsTable :items="allMotivations"/>
+  </main>
+</template>
