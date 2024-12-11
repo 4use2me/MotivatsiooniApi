eb5ad8c58d67714087ad1757e26f09ce85414bf4
WINDOWS-1DCGN01\Kasutaja <forusetome@gmail.com>
2024-12-11 16:50:07 +0200

    changeColumns(ID)Names

--- models/User.js
+++ models/User.js
@@ -6,6 +6,7 @@ module.exports = (sequelize, DataTypes) => {
                 type: DataTypes.INTEGER,
                 primaryKey: true,
                 autoIncrement: true,
+                field: "UserID",
             },
             UserName: {
                 type: DataTypes.STRING,
