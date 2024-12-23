a8bcc4e5b2ca9228f78885aecc146bd1f80967f0
WINDOWS-1DCGN01\Kasutaja <forusetome@gmail.com>
2024-12-23 11:21:11 +0200

    Delivers https://trello.com/c/ZY5fB9BV/37-as-a-user-or-admin-i-want-to-change-my-username-and-password
    An addition to the previous delivers (missed before)

--- routes/motivationRoutes.js
+++ routes/motivationRoutes.js
@@ -2,13 +2,15 @@ const MotivationsController = require ('../controllers/MotivationsController');
 const authenticate = require('../controllers/authMiddleware');
 
 module.exports = (app) => { 
-    app.route("/motivations")
-       .get(MotivationsController.getAll)   
-       .post(MotivationsController.create)
-    app.route('/motivations/user') // Motivatsioonid, mis kuuluvad sisseloginud kasutajale
-       .get(authenticate, MotivationsController.getUsersMotivations);
-    app.route("/motivations/:id")
-        .get(MotivationsController.getById)
-        .put(MotivationsController.editById)
-        .delete(MotivationsController.deleteById)
+   app.route("/motivations")
+      .get(MotivationsController.getAll)   
+      .post(MotivationsController.create)
+   app.route("/motivations/random")
+      .get(MotivationsController.getRandom)
+   app.route('/motivations/user') // Motivatsioonid, mis kuuluvad sisseloginud kasutajale
+      .get(authenticate, MotivationsController.getUsersMotivations);
+   app.route("/motivations/:id")
+      .get(MotivationsController.getById)
+      .put(MotivationsController.editById)
+      .delete(MotivationsController.deleteById)
 }
