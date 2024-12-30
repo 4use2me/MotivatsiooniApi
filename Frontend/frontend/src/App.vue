c90a586bb5d82fd4113587e8a0f883bce3dd583f
WINDOWS-1DCGN01\Kasutaja <forusetome@gmail.com>
2024-12-30 17:11:51 +0200

    Add nav and HomeView style from Pia

--- Frontend/frontend/src/App.vue
+++ Frontend/frontend/src/App.vue
@@ -9,7 +9,7 @@ import { RouterLink, RouterView } from 'vue-router';
       <nav>
         <RouterLink to="/">Motiveeri ennast!</RouterLink>
         <RouterLink to="/about">Meist</RouterLink>
-        <RouterLink class="login-nav" to="/auth">Registreeri/Logi sisse</RouterLink>
+        <RouterLink class="login-nav" to="/auth"><i class="fa-solid fa-user-plus"></i></RouterLink>
       </nav>
     </div>
   </header>
@@ -37,18 +37,41 @@ nav a:first-of-type {
   border: 0;
 }
 
-@media (min-width: 1024px) {
+nav .login-nav {
+  padding: 0.5rem 1rem;
+  background-color: var(--button-bg, #3f7d20);
+  color: var(--button-text, white);
+  border-radius: 5px;
+  text-decoration: none;
+  font-weight: bold;
+  display: inline-block;
+  margin-left: auto; /* Push it to the right */
+  margin-top: 6px;
+  margin-right: 10px;
+}
+
+nav .login-nav:hover {
+  background-color: var(--button-bg-hover, #18310c);
+  color: var(--button-text-hover, white);
+}
+nav {
+    display: flex;
+    align-items: center;
+    width: 100%;
+
+  }
+
+/* @media (min-width: 1024px) {
   header {
     display: flex;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
   }
 
-
   header .wrapper {
     display: flex;
     place-items: flex-start;
     flex-wrap: wrap;
   }
-}
+} */
 </style>
