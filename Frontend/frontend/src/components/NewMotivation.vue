52e9a9a0d1f41687bde4f41945ba5fffcc64099d
PiaLember <pia.lember@gmail.com>
2025-01-04 21:22:30 +0200

    Add style to LoggedInUser

--- Frontend/frontend/src/components/NewMotivation.vue
+++ Frontend/frontend/src/components/NewMotivation.vue
@@ -1,89 +1,102 @@
 <template>
-  <div>
-    <form @submit.prevent="submitMotivation">
-      <div>
-        <label for="quote">Tsitaat:</label>
-        <input
-          type="text"
-          id="quote"
-          v-model="quote"
-          placeholder="Sisesta tsitaat"
-          required
-        />
-      </div>
-      <button type="submit">Salvesta</button>
-      <p v-if="successMessage" class="success">{{ successMessage }}</p>
-      <p v-if="errorMessage" class="error">{{ errorMessage }}</p>
-    </form>
-  </div>
-</template>
+    <div class="new-quote">
+      <form  @submit.prevent="submitMotivation">
+        <div class="row g-3">
+          <div class="col input-group">
+            <span class="input-group-text">Tsitaat</span>
+            <textarea class="form-control" id="quote" rows="4" aria-label="With textarea" v-model="quote"
+              placeholder="Sisesta tsitaat" required >
+            </textarea>
+          </div>
+          <div class="col-6">
+            <button class="btn save-button" type="submit"><i class="fa-regular fa-floppy-disk"></i></button>
+          </div>
+        </div> 
 
-<script>
-import axios from 'axios';
-
-export default {
-  data() {
-    return {
-      quote: '', // Motivatsiooni tsitaat
-      successMessage: '', // Eduka tegevuse teade
-      errorMessage: '' // Veateade
-    };
-  },
-  methods: {
-    async submitMotivation() {
-      // Tühjenda varasemad teated enne uue taotluse esitamist
-      this.successMessage = '';
-      this.errorMessage = '';
-
-      const token = localStorage.getItem('token'); // Token localStorage'st
-      if (!token) {
-        this.errorMessage = 'Token puudub. Palun logi sisse.';
-        return;
-      }
-
-      const newMotivation = {
-        Quote: this.quote,
-        Likes: 1, // Meeldimiste arvu vaikimisi väärtus
+        <p v-if="successMessage" class="success">{{ successMessage }}</p>
+        <p v-if="errorMessage" class="error">{{ errorMessage }}</p>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        quote: '', // Motivatsiooni tsitaat
+        successMessage: '', // Eduka tegevuse teade
+        errorMessage: '' // Veateade
       };
-
-      try {
-        const response = await axios.post('http://localhost:8080/motivations', newMotivation, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-
-        if (response.status === 201) {
-          this.successMessage = 'Tsitaat lisatud edukalt!';
-          this.quote = ''; // Vorm tühjaks
-          this.$emit('motivationCreated'); // Emit sündmus vanemale
-        }
-        } catch (error) {
-        // Kui server tagastab veateate, kuvame selle
-        if (error.response && error.response.data.error) {
-          this.errorMessage = error.response.data.error; // Serveri veateade
-        } else {
-          this.errorMessage = 'Tsitaadi lisamine ebaõnnestus. Proovi uuesti.';
-        }
-      }
     },
-  },
-};
-</script>
+    methods: {
+      async submitMotivation() {
+        // Tühjenda varasemad teated enne uue taotluse esitamist
+        this.successMessage = '';
+        this.errorMessage = '';
+  
+        const token = localStorage.getItem('token'); // Token localStorage'st
+        if (!token) {
+          this.errorMessage = 'Token puudub. Palun logi sisse.';
+          return;
+        }
+  
+        const newMotivation = {
+          Quote: this.quote,
+          Likes: 1, // Meeldimiste arvu vaikimisi väärtus
+        };
+  
+        try {
+          const response = await axios.post('http://localhost:8080/motivations', newMotivation, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+  
+          if (response.status === 201) {
+            this.successMessage = 'Tsitaat lisatud edukalt!';
+            this.quote = ''; // Vorm tühjaks
+            this.$emit('motivationCreated'); // Emit sündmus vanemale
+          }
+          } catch (error) {
+          // Kui server tagastab veateate, kuvame selle
+          if (error.response && error.response.data.error) {
+            this.errorMessage = error.response.data.error; // Serveri veateade
+          } else {
+            this.errorMessage = 'Tsitaadi lisamine ebaõnnestus. Proovi uuesti.';
+          }
+        }
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+  button {
+    align-self: flex-start;
+  }
+  .success {
+    color: green;
+  }
+  .error {
+    color: red;
+  }
 
-<style scoped>
-form {
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-}
-button {
-  align-self: flex-start;
-}
-.success {
-  color: green;
-}
-.error {
-  color: red;
-}
-</style>
+  .save-button {
+      background-color: var(--button-bg, #3f7d20);
+      color: white;
+      box-shadow: 2px 2px 5px #18310c;
+    }
+  .save-button:hover{
+      background-color: var(--button-bg-hover, #18310c);
+    }
+  .new-quote{
+    margin-top: 10px;
+  }
+  </style>
--- Frontend/frontend/src/components/UpdateUser.vue
+++ Frontend/frontend/src/components/UpdateUser.vue
@@ -1,109 +1,133 @@
 <template>
-  <div>
-    <form @submit.prevent="submitEdit">
-      <label for="user">Muuda kasutajat:</label>
-      <input
-        type="text"
-        id="user"
-        v-model="updatedUser.UserName" 
-        required
-      />
-      <input
-        type="text"
-        id="user"
-        v-model="updatedUser.Password" 
-        required
-      />
-      <button type="submit">Salvesta</button>
-      <button type="button" @click="cancelEdit">Tühista</button>
-      <p v-if="error" class="error">{{ error }}</p>
-    </form>
-  </div>
-</template>
-
-<script>
-export default {
-  props: {
-    user: {
-      type: Object,
-      required: true,
-    },
-  },
-  data() {
-    return {
-      updatedUser: {}, // Algväärtusta tühjana
-      error: null, // Veateadete kuvamiseks
-    };
-  },
-  watch: {
-    user: {
-      immediate: true,
-      handler(newUser) {
-        // Uuenda updatedUser, kui props-i väärtus muutub
-        this.updatedUser = { ...newUser };
+    <div>
+      <form class="row g-3" @submit.prevent="submitEdit">
+        <div class="col input-group">
+          <span class="input-group-text">Muuda kasutajat</span>
+          <input type="text" id="user" class="form-control" v-model="updatedUser.UserName" 
+          required>
+          <input type="text" class="form-control" id="user"
+          v-model="updatedUser.Password" 
+          required>
+        </div>
+        <div class="col-6">
+          <button class="btn save-button" type="submit"><i class="fa-regular fa-floppy-disk"></i></button>
+          <button class="btn" type="button" @click="cancelEdit"><i class="fa-regular fa-rectangle-xmark"></i></button>
+        </div>
+        <p v-if="error" class="error">{{ error }}</p>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      user: {
+        type: Object,
+        required: true,
       },
     },
-  },
-  
-  methods: {
-    async submitEdit() {
-    this.error = null;
-
-    if (!this.updatedUser.UserName || !this.updatedUser.Password) {
-      this.error = "Kasutajanimi ja parool ei tohi olla tühjad.";
-      return;
-    }
-
-    if (!this.updatedUser.UserID) {
-      this.error = "Kasutaja ID puudub. Ei saa andmeid uuendada.";
-    return;
-  }
-
-    const token = localStorage.getItem("token");
-    if (!token) {
-      this.error = "Autoriseerimistoken puudub. Logige uuesti sisse.";
-      return;
-    }
-
-    try {
-      const { UserID, UserName, Password } = this.updatedUser;
-
-      console.log("Saadetav kasutaja:", { UserID, UserName, Password });
-
-      const response = await fetch(`http://localhost:8080/users/${UserID}`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify({ UserID, UserName, Password }),
-      });
-
-      if (response.ok) {
-        console.log("Kasutaja uuendatud:", await response.json());
-        this.$emit("userUpdated");
-      } else {
-        const errorData = await response.text(); // Logi serveri vastus
-        console.error("Serveri viga:", errorData);
-        this.error = "Kasutaja muutmine ebaõnnestus. Kontrolli serveri logisid.";
-      }
-      
-    } catch (error) {
-      console.error("Viga kasutaja muutmisel:", error);
-      this.error = "Serveri viga kasutaja muutmisel.";
-    }
-  },
-
-    cancelEdit() {
-      this.$emit("cancelEdit"); // Teavita vanemkomponenti, et muutmine katkestati
+    data() {
+      return {
+        updatedUser: {}, // Algväärtusta tühjana
+        error: null, // Veateadete kuvamiseks
+      };
     },
-  },
-};
-</script>
+    watch: {
+      user: {
+        immediate: true,
+        handler(newUser) {
+          // Uuenda updatedUser, kui props-i väärtus muutub
+          this.updatedUser = { ...newUser };
+        },
+      },
+    },
+    
+    methods: {
+      async submitEdit() {
+      this.error = null;
+  
+      if (!this.updatedUser.UserName || !this.updatedUser.Password) {
+        this.error = "Kasutajanimi ja parool ei tohi olla tühjad.";
+        return;
+      }
+  
+      if (!this.updatedUser.UserID) {
+        this.error = "Kasutaja ID puudub. Ei saa andmeid uuendada.";
+      return;
+    }
+  
+      const token = localStorage.getItem("token");
+      if (!token) {
+        this.error = "Autoriseerimistoken puudub. Logige uuesti sisse.";
+        return;
+      }
+  
+      try {
+        const { UserID, UserName, Password } = this.updatedUser;
+  
+        console.log("Saadetav kasutaja:", { UserID, UserName, Password });
+  
+        const response = await fetch(`http://localhost:8080/users/${UserID}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify({ UserID, UserName, Password }),
+        });
+  
+        if (response.ok) {
+          console.log("Kasutaja uuendatud:", await response.json());
+          this.$emit("userUpdated");
+        } else {
+          const errorData = await response.text(); // Logi serveri vastus
+          console.error("Serveri viga:", errorData);
+          this.error = "Kasutaja muutmine ebaõnnestus. Kontrolli serveri logisid.";
+        }
+        
+      } catch (error) {
+        console.error("Viga kasutaja muutmisel:", error);
+        this.error = "Serveri viga kasutaja muutmisel.";
+      }
+    },
+  
+      cancelEdit() {
+        this.$emit("cancelEdit"); // Teavita vanemkomponenti, et muutmine katkestati
+      },
+    },
+  };
+  </script>
+  
 
-<style>
-.error {
-  color: red;
-  font-weight: bold;
+  
+  <style scoped>
+  .error {
+    color: red;
+    font-weight: bold;
+  }
+.btn {
+    background-color: var(--vt-c-dark);
+    box-shadow: 2px 2px 5px var(--vt-c-dark);
+    color: white;
+    margin-right: 6px;
+    font-size: x-large;
+    padding: 0px 6px 0px 6px;
+  }
+.btn:hover {
+    background-color: #51294D;
+  }
+.quote{
+  display: flex;
 }
+
+.save-button {
+      background-color: var(--button-bg, #3f7d20);
+      color: white;
+      box-shadow: 2px 2px 5px #18310c;
+    }
+    .save-button:hover{
+      background-color: var(--button-bg-hover, #18310c);
+    }
 </style>
+
+  
